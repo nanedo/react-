22319db1f95e98a2155f162c0154f58d15c96f20
nanedo <toms0825@gmail.com>
2018-04-11 17:44:25 +0800

    react router

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
 import Clock from './Clock.jsx'
 
 import './index.scss'
@@ -66,7 +67,38 @@ let style = {
 let name = "nanedo"
 let arr = [1, 2, 3]
 
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>Components</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic} />
+    <Route
+      exact
+      path={match.url}
+      render={() => <h3>Please select a topic.</h3>}
+    />
+  </div>
+);
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+);
+
 let myjsx = (
+  <Router>
 <div>
   <h1 className="mylo">hello6 { Math.random() > 0.5 ? name : (<span>abcde</span>)}</h1>
   <h2 style={style}>23424324</h2>
@@ -75,15 +107,29 @@ let myjsx = (
   <ul>
   { arr.map((item, index) => (<li key={index}> {item} </li>) )}
   </ul>
-  
-  <MyComponent />
-  <TheComponent>
-    <h2>容器式的组件</h2>
-  </TheComponent>
+  <ul>
+      <li>
+        <Link to="/dist/index">Home</Link>
+      </li>
+      <li>
+        <Link to="/dist/about" exact >About</Link>
+      </li>
+      <li>
+        <Link to="/dist/topics">Topics</Link>
+      </li>
+    </ul>
+  <Route 
+    exact 
+    path="/dist/index" 
+    render={(props) => (
+      <Child1 {...props} onNameChange={(e)=>alert(e)}  />
+    )} 
+  />
+  <Route path="/dist/about" component={TheComponent} />
+  <Route path="/dist/topics" component={Topics} />
 </div>
+</Router>
 );
-
-
 
 ReactDOM.render(
   myjsx,
